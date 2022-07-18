d1a500d5129f8a9d38b463b0dfdcca6e25191681
formk <formk@kermed.net>
2022-07-18 11:44:51 +0200

    utilisation de map pour parcourir des listes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const message = <h1>Hello le monde</h1>;
+  const message2 = (
+    <div>
+      <h3>Block html</h3>
+      <p>Lorem ipsum dolor sit amet consectetur.</p>
+    </div>
+  );
+  const calcul = <h2>Cette operation 2 * 6 = {2 * 6}</h2>;
+  const loggedIn = true;
+  const voitures = ["Ford", "Audi", "Honda"];
+  const voitures2 = [
+    { id: 1, nom: "Ford" },
+    { id: 2, nom: "Audi" },
+    { id: 3, nom: "Honda" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {message}
+      {message2}
+      {calcul}
+      {loggedIn && <p>Vous etes connecte</p>}
+      <ul>
+        {voitures.map((voiture) => (
+          <li key={voiture}>{voiture}</li>
+        ))}
+      </ul>
+      <ul>
+      {voitures2.map((voiture) => (
+        <li key={voiture.id}>{voiture.nom}</li>
+      ))}  
+      </ul>    
     </div>
   );
 }
