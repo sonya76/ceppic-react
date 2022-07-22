30f7ba76ea164da7f1889367a7bd1e0a3fa1493b
formk <formk@kermed.net>
2022-07-22 15:30:13 +0200

    recuperation de donne via une api

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import "./App.css";
 
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import RandomUser from "./components/RandomUser";
 import Users from "./components/Users";
 
-
 function App() {
-
-    return (
+  return (
     <div className="App">
-        <Header title="React JS" texte="librairie javascript fonde par Facebook" />
-     <Users />
+      <Header
+        title="React JS"
+        texte="librairie javascript fonde par Facebook"
+      />
+
+      <RandomUser />
+
+      {/* <Users /> */}
 
       <Footer />
     </div>
