8fd309ba9d2efa6c0772604afb042b2f1a7f4133
formk <formk@kermed.net>
2022-07-22 16:56:18 +0200

    ajout de react-routeur-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,31 @@ import "./App.css";
 
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import RandomUser from "./components/RandomUsers";
-import Users from "./components/Users";
+// import RandomUser from "./components/RandomUsers";
+// import Users from "./components/Users";
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+
       <Header
         title="React JS"
         texte="librairie javascript fonde par Facebook"
       />
 
-      <RandomUser />
+      {/* <RandomUser /> */}
 
       {/* <Users /> */}
 
